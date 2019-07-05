7399cea947436728f4cea2b16dc98b412b8d09e6
Richard Aguirre <richardjap@gmail.com>
2019-07-05 05:45:33 -0500

    cart

--- angular.js
+++ angular.js
@@ -16,7 +16,21 @@ angular.module("Hackaton",[])
 
 }).controller("CatalogSeller",function($scope){
 
- 
+  $scope.goToSelection=function(){
+    window.location="/selection.html";
+  }
   
+
+}).controller("SelectionController",function($scope,$http){
+
+$http.get("products.json").then(function(data){
+  data.data.forEach(element => {
+    element.imagenx="https://s3.amazonaws.com/catalogodigitalbelcorp/PE/201818/"+ element.codigo_marca+"/productos/" + element.imagen; 
+  });
+  $scope.products=data.data.slice(3,12);
+});
+
+
+
 
 })
